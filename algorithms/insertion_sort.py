fc623576ddf8a623684563de0ced3eaac5c144eb
Nikita Danylov <mdanyl@softserveinc.com>
2017-04-01 15:54:54 +0300

    recursive version of the insertion sort

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -16,3 +16,18 @@ def sort_in_non_increasing_mode(keys, keys_length):
             keys[i + 1] = keys[i]
             i -= 1
         keys[i + 1] = key
+
+
+def insertion_sort_rec(keys):
+    if len(keys) == 1:
+        return keys
+    key = keys[-1]
+    j = len(keys)-1
+    res = insertion_sort_rec(keys[:j])
+    res.append(key)
+    i = j - 1
+    while i >= 0 and key < res[i]:
+        res[i+1] = res[i]
+        i -= 1
+    res[i+1] = key
+    return res
