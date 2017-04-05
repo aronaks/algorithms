50e10474e76090d363b6efca13fcb019088e97f2
Nikita Danylov <mdanyl@softserveinc.com>
2017-04-05 22:25:15 +0300

    add bubble sort

---
+++ algorithms/buble_sort.py
@@ -0,0 +1,5 @@
+def bubble_sort(keys, keys_length):
+    for i in range(0, keys_length):
+        for j in range(keys_length-1, i, -1):
+            if keys[j] < keys[j-1]:
+                keys[j], keys[j-1] = keys[j-1], keys[j]
---
+++ tests/test_bubble_sort.py
@@ -0,0 +1,8 @@
+from algorithms.buble_sort import bubble_sort
+
+
+def test_bubble_sort():
+    keys = list(range(100, 0, -1))
+    bubble_sort(keys, len(keys))
+    expected_result = sorted(keys)
+    assert keys == expected_result
