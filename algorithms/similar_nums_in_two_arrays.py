12d938a4a52c48ddd50e31d8021f0e9a908695c3
Mykyta Danylov <mdanyl@macmini1207.local>
2017-04-13 16:30:41 +0300

    do not store lengths in variables

--- algorithms/similar_nums_in_two_arrays.py
+++ algorithms/similar_nums_in_two_arrays.py
@@ -6,13 +6,9 @@ def count_similar_nums(keys1, keys2):
     There are two arrays that are both sorted. I want you to design an algorithm
     to count the number of elements that these two arrays have in common.
     """
-    keys1_length = len(keys1)
-    keys2_length = len(keys2)
-
     i = j = 0
-
     common_elements_num = 0
-    while i < keys1_length and j < keys2_length:
+    while i < len(keys1) and j < len(keys2):
         if keys1[i] > keys2[j]:
             j += 1
         elif keys1[i] < keys2[j]:
