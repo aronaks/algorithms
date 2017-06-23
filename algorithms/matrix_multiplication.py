4e677a31df57fb8fda13e6ebff4a7498bb5fc0fd
Nikita Danylov <mdanyl@softserveinc.com>
2017-06-23 15:18:10 +0300

    matrix multiplication

--- algorithms/matrix_multiplication.py
+++ algorithms/matrix_multiplication.py
@@ -1,14 +1,57 @@
+from numpy import array
+
 # O(n^3)
 
 
 def multiply_matrices_brute_force(a, b):
-    if len(a) != len(b[0]):
+    a_rows = a.shape[0]
+    a_cols = a.shape[1]
+    b_rows = b.shape[0]
+    b_cols = b.shape[1]
+    if a_cols != b_rows:
         return 0
-    n = len(a)
-    c = [[None for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
+    c = array([[None for _ in range(a_rows)] for _ in range(b_cols)])
+    for i in range(a_cols):
+        for j in range(a_cols):
             c[i][j] = 0
-            for k in range(n):
+            for k in range(a_cols):
                 c[i][j] += a[i][k] * b[k][j]
+    return c
+
+
+def multiply_square_matrices(a, b):
+    a_rows = a.shape[0]
+    a_cols = a.shape[1]
+    b_rows = b.shape[0]
+    b_cols = b.shape[1]
+    if a_cols != b_rows:
+        return 0
+    c = array([[None for _ in range(a_rows)] for _ in range(b_cols)])
+    if a_rows == 1 or b_rows == 1:
+        c = a @ b
+    else:
+        a_rows_divided = int(a_rows / 2)
+        a_cols_divided = int(a_cols / 2)
+        b_rows_divided = int(b_rows / 2)
+        b_cols_divided = int(b_cols / 2)
+
+        c[0:a_rows_divided, 0:b_cols_divided] = multiply_square_matrices(
+            a[0:a_rows_divided, 0:a_cols_divided], b[0:b_rows_divided, 0:b_cols_divided]) + multiply_square_matrices(
+            a[0:a_rows_divided, a_cols_divided:a_cols], b[b_rows_divided:b_rows, 0:b_cols_divided])
+
+        c[0:a_rows_divided, b_cols_divided:b_cols] = (multiply_square_matrices(
+            a[0:a_rows_divided, 0:a_cols_divided], b[0:b_rows_divided, b_cols_divided:b_cols])
+                                                      + multiply_square_matrices(
+            a[0:a_rows_divided, a_cols_divided:a_cols], b[b_rows_divided:b_rows, b_cols_divided:b_cols]))
+
+        c[a_rows_divided:a_rows, 0:b_cols_divided] = (multiply_square_matrices(
+            a[a_rows_divided:a_rows, 0:a_cols_divided], b[0:b_rows_divided, 0:b_cols_divided])
+                                                      + multiply_square_matrices(
+            a[a_rows_divided:a_rows, a_cols_divided:a_cols], b[b_rows_divided:b_rows, 0:b_cols_divided]))
+
+        c[a_rows_divided:a_rows, b_cols_divided:b_cols] = (multiply_square_matrices(
+            a[a_rows_divided:a_rows, 0:a_cols_divided], b[0:b_rows_divided, b_cols_divided:b_cols]) +
+                                                           multiply_square_matrices(
+            a[a_rows_divided:a_rows, a_cols_divided:a_cols], b[b_rows_divided:b_rows, b_cols_divided:b_cols]))
+
     return c
--- tests/test_matrix_multiplication.py
+++ tests/test_matrix_multiplication.py
@@ -1,8 +1,25 @@
-from algorithms.matrix_multiplication import multiply_matrices_brute_force
+from numpy import *
+from numpy.testing import assert_array_equal
+
+from algorithms.matrix_multiplication import multiply_matrices_brute_force, multiply_square_matrices
 
 
 def test_multiply_matrices_brute_force():
-    a = [[1, 2], [3, 4]]
-    b = [[5, 6], [7, 8]]
+    a = array([[1, 2], [3, 4]])
+    b = array([[5, 6], [7, 8]])
     res = multiply_matrices_brute_force(a, b)
-    assert res == [[19, 22], [43, 50]]
+    assert_array_equal(res, array([[19, 22], [43, 50]]))
+
+
+def test_multiply_matrices_recursively_case1():
+    a = array([[1, 2], [3, 4]])
+    b = array([[5, 6], [7, 8]])
+    res = multiply_square_matrices(a, b)
+    assert_array_equal(res, array([[19, 22], [43, 50]]))
+
+
+def test_multiply_matrices_recursively_case2():
+    a = array([[1, 2, 3], [4, 5, 6]])
+    b = array([[7, 8], [9, 10], [11, 12]])
+    res = multiply_square_matrices(a, b)
+    assert_array_equal(res, array([[58, 64], [139, 154]]))
