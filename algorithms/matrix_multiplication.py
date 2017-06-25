64e3949b0da122ac799261f0aae9e14ed461de8b
Nikita Danylov <mdanyl@softserveinc.com>
2017-06-25 17:14:07 +0300

    add strassen alg

--- algorithms/matrix_multiplication.py
+++ algorithms/matrix_multiplication.py
@@ -55,3 +55,46 @@ def multiply_square_matrices(a, b):
             a[a_rows_divided:a_rows, a_cols_divided:a_cols], b[b_rows_divided:b_rows, b_cols_divided:b_cols]))
 
     return c
+
+
+def multiply_matrices_strassen(a, b):
+    a_rows = a.shape[0]
+    a_cols = a.shape[1]
+    b_rows = b.shape[0]
+    b_cols = b.shape[1]
+    if a_cols != b_rows:
+        return 0
+    c = array([[None for _ in range(a_rows)] for _ in range(b_cols)])
+    if a_rows == 1 or b_rows == 1:
+        c = a @ b
+    else:
+        a_rows_divided = int(a_rows / 2)
+        a_cols_divided = int(a_cols / 2)
+        b_rows_divided = int(b_rows / 2)
+        b_cols_divided = int(b_cols / 2)
+
+        s1 = b[0:b_rows_divided, b_cols_divided:b_cols] - b[b_rows_divided:b_rows, b_cols_divided:b_cols]
+        s2 = a[0:a_rows_divided, 0:a_cols_divided] + a[0:a_rows_divided, a_cols_divided:a_cols]
+        s3 = a[a_rows_divided:a_rows, 0:a_cols_divided] + a[a_rows_divided:a_rows, a_cols_divided:a_cols]
+        s4 = b[b_rows_divided:b_rows, 0:b_cols_divided] - b[0:b_rows_divided, 0:b_cols_divided]
+        s5 = a[0:a_rows_divided, 0:a_cols_divided] + a[a_rows_divided:a_rows, a_cols_divided:a_cols]
+        s6 = b[0:b_rows_divided, 0:b_cols_divided] + b[b_rows_divided:b_rows, b_cols_divided:b_cols]
+        s7 = a[0:a_rows_divided, a_cols_divided:a_cols] - a[a_rows_divided:a_rows, a_cols_divided:a_cols]
+        s8 = b[b_rows_divided:b_rows, 0:b_cols_divided] + b[b_rows_divided:b_rows, b_cols_divided:b_cols]
+        s9 = a[0:a_rows_divided, 0:a_cols_divided] - a[a_rows_divided:a_rows, 0:a_cols_divided]
+        s10 = b[0:b_rows_divided, 0:b_cols_divided] + b[0:b_rows_divided, b_cols_divided:b_cols]
+
+        p1 = multiply_matrices_strassen(a[0:a_rows_divided, 0:a_cols_divided], s1)
+        p2 = multiply_matrices_strassen(s2, b[b_rows_divided:b_rows, b_cols_divided:b_cols])
+        p3 = multiply_matrices_strassen(s3, b[0:b_rows_divided, 0:b_cols_divided])
+        p4 = multiply_matrices_strassen(a[a_rows_divided:a_rows, a_cols_divided:a_cols], s4)
+        p5 = multiply_matrices_strassen(s5, s6)
+        p6 = multiply_matrices_strassen(s7, s8)
+        p7 = multiply_matrices_strassen(s9, s10)
+
+        c[0:a_rows_divided, 0:b_cols_divided] = p5 + p4 - p2 + p6
+        c[0:a_rows_divided, b_cols_divided:b_cols] = p1 + p2
+        c[a_rows_divided:a_rows, 0:b_cols_divided] = p3 + p4
+        c[a_rows_divided:a_rows, b_cols_divided:b_cols] = p5 + p1 - p3 - p7
+
+    return c
--- tests/test_matrix_multiplication.py
+++ tests/test_matrix_multiplication.py
@@ -1,7 +1,8 @@
 from numpy import *
 from numpy.testing import assert_array_equal
 
-from algorithms.matrix_multiplication import multiply_matrices_brute_force, multiply_square_matrices
+from algorithms.matrix_multiplication import (multiply_matrices_brute_force, multiply_square_matrices,
+                                              multiply_matrices_strassen)
 
 
 def test_multiply_matrices_brute_force():
@@ -23,3 +24,11 @@ def test_multiply_matrices_recursively_case2():
     b = array([[7, 8], [9, 10], [11, 12]])
     res = multiply_square_matrices(a, b)
     assert_array_equal(res, array([[58, 64], [139, 154]]))
+
+
+def test_multiply_matrices_strassen():
+    a = array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+    b = array([[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32]])
+    res = multiply_matrices_strassen(a, b)
+    assert_array_equal(res, array([[250, 260, 270, 280], [618, 644, 670, 696], [986, 1028, 1070, 1112],
+                                   [1354, 1412, 1470, 1528]]))
